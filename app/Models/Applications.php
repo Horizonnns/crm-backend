503ec44b23e37f2b963b1d8bec5c6655f85973ed
Horizonnns <for.office.gthb@gmail.com>
2024-01-02 08:52:08 +0500

    Created table for applications

---
+++ app/Models/Applications.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Applications extends Model
+{
+    use HasFactory, Notifiable;
+
+        protected $fillable = [
+        'specialist_name',
+        'phonenum',
+        'topic',
+        'account_number',
+        'createddate',
+        'comment',
+        'job_title',
+    ];
+}
