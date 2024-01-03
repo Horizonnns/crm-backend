c1ca0401668ff1cb7aa1987e989d8667905f15d4
Horizonnns <for.office.gthb@gmail.com>
2024-01-03 14:09:04 +0500

    Moved the routes to the middleware in the API

--- routes/api.php
+++ routes/api.php
@@ -13,24 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// User routes
+Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
+Route::post('authAdmin', [\App\Http\Controllers\AuthController::class, 'authAdmin']);
+
+
+Route::middleware('auth:sanctum')->group(function() {
+Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
+Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+
+
+// User routes
+Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']);
+Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']);
+Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']);
+
+
 // Application routes
 Route::get('getAllApps', [\App\Http\Controllers\FrontController::class, 'getAllApps']);
 Route::post('createApp', [\App\Http\Controllers\FrontController::class, 'createApp']);
 Route::delete('deleteApp/{id}', [\App\Http\Controllers\FrontController::class, 'deleteApp']);
 Route::post('searchApps', [\App\Http\Controllers\FrontController::class, 'searchApps']);
 Route::put('updateApp/{id}', [\App\Http\Controllers\FrontController::class, 'updateApp']);
-
-
-
-// User routes
-Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']);
-Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']);
-Route::post('authAdmin', [\App\Http\Controllers\AuthController::class, 'authAdmin']);
-Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
-Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']);
-
-
-Route::middleware('auth:sanctum')->group(function() {
-Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
-Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 });
