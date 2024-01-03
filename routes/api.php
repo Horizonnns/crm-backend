58daa3ada43153d4a045e7e58aa5669eea438bfc
Horizonnns <for.office.gthb@gmail.com>
2024-01-04 01:22:53 +0500

    Fixed middleware routes in API

--- routes/api.php
+++ routes/api.php
@@ -16,23 +16,20 @@ use Illuminate\Support\Facades\Route;
 // User routes
 Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('authAdmin', [\App\Http\Controllers\AuthController::class, 'authAdmin']);
+Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']);
+Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']);
+Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']);
+
+
+// Application routes
+Route::post('createApp', [\App\Http\Controllers\FrontController::class, 'createApp']);
+Route::delete('deleteApp/{id}', [\App\Http\Controllers\FrontController::class, 'deleteApp']);
+Route::get('getAllApps', [\App\Http\Controllers\FrontController::class, 'getAllApps']);
+Route::post('searchApps', [\App\Http\Controllers\FrontController::class, 'searchApps']);
+Route::put('updateApp/{id}', [\App\Http\Controllers\FrontController::class, 'updateApp']);
 
 
 Route::middleware('auth:sanctum')->group(function() {
 Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
 Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
-
-
-// User routes
-Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']);
-Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']);
-Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']);
-
-
-// Application routes
-Route::get('getAllApps', [\App\Http\Controllers\FrontController::class, 'getAllApps']);
-Route::post('createApp', [\App\Http\Controllers\FrontController::class, 'createApp']);
-Route::delete('deleteApp/{id}', [\App\Http\Controllers\FrontController::class, 'deleteApp']);
-Route::post('searchApps', [\App\Http\Controllers\FrontController::class, 'searchApps']);
-Route::put('updateApp/{id}', [\App\Http\Controllers\FrontController::class, 'updateApp']);
 });
