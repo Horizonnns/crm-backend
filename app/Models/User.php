c8b631f836d86a4c6d31adc106733874966706fc
Horizonnns <for.office.gthb@gmail.com>
2024-01-14 19:26:43 +0500

    Changed the date and time format during user creation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,22 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
+use Carbon\Carbon;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable, HasApiTokens, HasRoles;
+
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d',
+    ];
+
+    public function toArray()
+    {
+        $array = parent::toArray();
+        $array['created_at'] = Carbon::parse($this->created_at)->format('Y-m-d H:i');
+        return $array;
+    }
 
     protected $fillable = [
         'name',
