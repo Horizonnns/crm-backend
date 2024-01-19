5538693bcfcde05fd69a64d76e49e36834f18d48
Horizonnns <for.office.gthb@gmail.com>
2024-01-19 16:32:24 +0500

    Added all routes in the middleware for token access

--- routes/api.php
+++ routes/api.php
@@ -13,24 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// User routes
+
+// <-- Auth routes -->
 Route::get('login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('authAdmin', [\App\Http\Controllers\AuthController::class, 'authAdmin']);
-Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']);
-Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']);
-Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']);
-
-
-// Application routes
-Route::post('createApp', [\App\Http\Controllers\FrontController::class, 'createApp']);
-Route::delete('deleteApp/{id}', [\App\Http\Controllers\FrontController::class, 'deleteApp']);
-Route::get('getAllApps', [\App\Http\Controllers\FrontController::class, 'getAllApps']);
-Route::post('searchApps', [\App\Http\Controllers\FrontController::class, 'searchApps']);
-
 
 Route::middleware('auth:sanctum')->group(function() {
-Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']);
-Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
+Route::get('user', [\App\Http\Controllers\AuthController::class, 'user']); // get user
+Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']); // logout
 
-Route::put('updateApp/{id}', [\App\Http\Controllers\FrontController::class, 'updateApp'])->middleware('back-office');
+
+// <-- User routes -->
+Route::post('authUser', [\App\Http\Controllers\AuthController::class, 'authUser']); // create user
+Route::put('updateUser/{id}', [\App\Http\Controllers\AuthController::class, 'updateUser']); // update user
+Route::delete('deleteUser/{id}', [\App\Http\Controllers\AuthController::class, 'deleteUser']); // delete user
+
+
+// <-- Application routes -->
+Route::post('createApp', [\App\Http\Controllers\FrontController::class, 'createApp']); // create app
+Route::put('updateApp/{id}', [\App\Http\Controllers\FrontController::class, 'updateApp'])->middleware('back-office'); // update app
+Route::delete('deleteApp/{id}', [\App\Http\Controllers\FrontController::class, 'deleteApp']); // delete app
+Route::get('getAllApps', [\App\Http\Controllers\FrontController::class, 'getAllApps']); // get all apps
+Route::post('searchApps', [\App\Http\Controllers\FrontController::class, 'searchApps']); // search apps
+});
+
+
 });
